431b2e3b1b61f92a32ea42699f6941fe8425ad01
Jason Fitzgerald <jfitzgerald@csumb.edu>
2016-12-06 00:25:26 -0800

    Update mp3Player.py
    got more stuff to work, still need to fix progress bar, picking different songs, from the list, maybe a background image.

--- mp3Player.py
+++ mp3Player.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import *
 from PIL import Image, ImageTk
+import numpy as np
 import pygame
 import sys
 from pygame.locals import *
@@ -10,9 +11,9 @@ from pygame.locals import *
 import os, shutil
 
 LARGE_FONT = ("Verdana", 12)
-filePath = []
+fileLabel = []
 filelist = []
-radio = []
+filebase = []
 value = 0
 x = 0
 song = 0
@@ -36,6 +37,7 @@ class MP3Player(tk.Tk):
         self.frames[f] = frame
         frame.grid(row = 0, column = 0, sticky = "nsew")
 
+
         self.show_frame(StartPage)
     def show_frame(self,cont):
         frame = self.frames[cont]
@@ -44,23 +46,24 @@ class MP3Player(tk.Tk):
 class StartPage(tk.Frame):
 
     def __init__(self,parent,controller):
-
-        def Fn(self, v):
-            result = self._oldvalue
-            self._oldvalue = v
-            pygame.mixer.music.load(filePath[result])
-            print(filePath[result])
+        pygame.init()
+        global value, song
 
         def Play():
-            print (filePath[song])
             print("play")
-            # pygame.mixer.music.load('/Users/jasonfitzgerald/Desktop/cst205/project3/bma.mp3')
-
             pygame.mixer.music.play()
+            pg.start()
+            pg.configure( mode='determinate', value = pygame.mixer.music.get_pos())
+
+
+
 
         def Pause():
             print("Pause")
             pygame.mixer.music.pause()
+            pg.stop()
+
+
 
         def Horror():
             print("Prepare for the horror")
@@ -74,37 +77,35 @@ class StartPage(tk.Frame):
         def Emo():
             print("Prepare for some emo tunes.")
         def Browse():
-            global filePath
+            global fileLabel
             global x
             x = 0
             del filelist[:]
+            del filebase[:]
             filename = filedialog.askopenfilename()
             filelist.append(filename)
-            for i in filelist:
-                filePath.append(Label(self,text= i, font = LARGE_FONT))
-            #     radio.append(Radiobutton(self,value = i,variable = i))
-            # for j in radio:
-            #     j.place(x = 180, y = 250+(x*30))
-            #     x = x + 1
-            for j in filePath:
+            base=os.path.basename(filename)
+            filebase.append(base)
+            for i in filebase:
+                fileLabel.append(Label(self,text= i, font = LARGE_FONT))
+            for j in fileLabel:
                 j.place(x = 200, y = 250+(x*30))
                 x = x + 1
-            # print (filePath)
-            # print (filelist)
-            print('\n\n')
+            labelBox = tk.Label(self, text = "Pick a song: ", font = LARGE_FONT)
+            labelBox.place(x = 40, y = 280)
 
-            labelM = tk.Label(self, text = "Pick a song: ", font = LARGE_FONT)
-            labelM.place(x = 40, y = 280)
-
-            SpinBox  = Spinbox( self,from_ = 0, to = len(filePath), command=lambda:self.Fn(val.get()), width = 5 )
+            SpinBox = Spinbox( self,from_ = 0, to = len(fileLabel), width = 5 , command = song)
             SpinBox.place (x = 120, y = 280)
+            print (song)
+            pygame.mixer.music.load(filelist[song])
 
-
+        def slider():
+            vol = sliderlength/100
+            pygame.mixer.music.set_volume(vol)
 
 
 
         tk.Frame.__init__(self,parent)
-
         labelStartStop = tk.Label(self, text = "Press start to play audio. Press pause to stop audio.", font = LARGE_FONT)
         labelStartStop.place(x = 20, y = 20)
 
@@ -117,8 +118,9 @@ class StartPage(tk.Frame):
         pause = tk.Button(self,text = "Pause", command = Pause)
         pause.place(x = 160, y = 60)
 
-        pg = ttk.Progressbar(self, orient='horizontal',length=300, mode='determinate')
+        pg = ttk.Progressbar(self, orient='horizontal',length=300, mode='determinate' )
         pg.place(x = 40, y = 110)
+
 
         FileBrowse = tk.Button(self,text = "Browse for Music",command = Browse)
         FileBrowse.place(x = 40, y = 250)
@@ -141,13 +143,22 @@ class StartPage(tk.Frame):
         label = tk.Label(self,text = "Volume", font = LARGE_FONT)
         label.place(x = 350, y = 20)
 
-        Slider1 = Scale(self, orient = HORIZONTAL, length = 100,width = 10, sliderlength = 10, from_ = 0, to = 100)
+        Slider1 = Scale(self, orient = HORIZONTAL, length = 100,width = 10, sliderlength = 10, from_ = 0, to = 100, command = slider)
         Slider1.place (x = 350, y = 40)
 
+        #pygame.mixer.music.set_volume(set)
+
+        # image = Image.open("/Users/jasonfitzgerald/Desktop/cst205/project3/backgroundImage.jpg")
+        # photo = ImageTk.PhotoImage(image)
+        #
+        # label = Label(image = photo)
+        # label.image = photo # keep a reference!
+        # label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-        # background_image = PhotoImage('/Users/jasonfitzgerald/Desktop/cst205/project3/backgroundImage.jpg')
-        # background = Label(self, image=background_image, bd=0)
+
+        # background_image = Image.open('/Users/jasonfitzgerald/Desktop/cst205/project3/backgroundImage.jpg')
+        # background = tk.Label(self, image = background_image)
         # background.place(x=0, y=0, relwidth=1, relheight=1)
 
         # self.image = tk.PhotoImage('/Users/jasonfitzgerald/Desktop/cst205/project3/backgroundImage.jpg')
@@ -156,23 +167,28 @@ class StartPage(tk.Frame):
 
         # im = Image.open('/Users/jasonfitzgerald/Desktop/cst205/project3/backgroundImage.jpg')
         # tkimage = ImageTk.PhotoImage(im)
-        # myvar = tk.Label(self,image = tkimage)
+        # myvar = Label(image = tkimage)
         # myvar.place(x=0, y=0, relwidth=1, relheight=1)
 
-def Audio():
-    pygame.init()  # turn all of pyGame on.
-    # According to documentation, you should use OGG or WAV sound files.
-    pygame.mixer.init(44100, -16,2,2048)
-    # pygame.mixer.pre_init(44100, 16, 2, 2048)
-    #  frequency, size, channels, bufferSize
-    # pygame.mixer.music.load('C:/tmp/MediaPLayer/BMA.wav')
-    # pygame.mixer.music.load('/Users/jasonfitzgerald/Desktop/cst205/project3/bma.mp3')
-    clock = pygame.time.Clock()
-    clock.tick(10)
-    while pygame.mixer.music.get_busy():
-        pygame.event.poll()
-        clock.tick(10)
-        pygame.mixer.music.set_volume(vol)
+
+
+
+# def Audio():
+#     pygame.init()
+#     pygame.mixer.init(44100, -16,2,2048)
+#     # pygame.mixer.pre_init(44100, 16, 2, 2048)
+#     #  frequency, size, channels, bufferSize
+#     # pygame.mixer.music.load('C:/tmp/MediaPLayer/BMA.wav')
+#
+#     # pygame.mixer.music.load(fileLabel[])
+#     # pygame.mixer.music.load('/Users/jasonfitzgerald/Desktop/cst205/project3/welcometotheplanet.wav')
+#     clock = pygame.time.Clock()
+#     clock.tick(10)
+#     while pygame.mixer.music.get_busy():
+#         pygame.event.poll()
+#         clock.tick(10)
+#         pygame.mixer.music.set_volume(vol)
 app = MP3Player()
-app.geometry("800x400")
+app.geometry("600x400")
 app.mainloop()
+
